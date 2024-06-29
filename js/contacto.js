1c647141d0225bf88fa4e45fc4c6d5a5e7db1eaa
Marcelo Farias <hmfarias7@gmail.com>
2024-06-29 15:38:24 -0300

    Initial state

---
+++ js/contacto.js
@@ -0,0 +1,9 @@
+//bring the form
+const FORMULARIO = document.querySelector('#contactForm');
+
+FORMULARIO.addEventListener('submit', handleSubmit);
+
+// Function to handle the form
+function handleSubmit(event) {
+	// event.preventDefault();
+}
