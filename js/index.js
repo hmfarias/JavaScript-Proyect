273f44b882a38c18a3e9e5ec1e6bab80cd8cb54d
Marcelo Farias <hmfarias7@gmail.com>
2024-07-18 09:15:39 -0300

    2 minutes timeout added outside ADMINISTRACION

--- js/index.js
+++ js/index.js
@@ -1,60 +1,66 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const loginButton = document.getElementById('loginButton');
-	const logoutButton = document.getElementById('logoutButton');
-	const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
-	const loginForm = document.getElementById('loginForm');
-	const navItemAdmin = document.getElementById('nav-item-admin');
+import { inactivityTime } from './functions.js';
 
-	// Primero chequeo es estatus de login desde el local storage
-	// si está logeado habilito el menú de Administración y desabilito el Login
-	const loginStatus = localStorage.getItem('login');
-	if (loginStatus) {
-		navItemAdmin.classList.remove('disabled');
-		navItemAdmin.classList.add('enabled');
-		loginButton.classList.remove('enabled');
-		loginButton.classList.add('disabled');
-	}
+// document.addEventListener('DOMContentLoaded', () => {
+const loginButton = document.getElementById('loginButton');
+const logoutButton = document.getElementById('logoutButton');
+const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
+const loginForm = document.getElementById('loginForm');
+const navItemAdmin = document.getElementById('nav-item-admin');
 
-	//Se abre la ventana modal cuando se hace click en la opción del menú "Login"
-	loginButton.addEventListener('click', () => {
-		loginModal.show();
-	});
+// Primero chequeo es estatus de login desde el local storage
+// si está logeado habilito el menú de Administración y desabilito el Login
+const loginStatus = localStorage.getItem('login');
+if (loginStatus) {
+	navItemAdmin.classList.remove('disabled');
+	navItemAdmin.classList.add('enabled');
+	loginButton.classList.remove('enabled');
+	loginButton.classList.add('disabled');
+	// Llamo a la función que controla inactividad
+	inactivityTime();
+}
 
-	//Acá se hace el manejo del formulario de login, al hacer submit
-	loginForm.addEventListener('submit', (event) => {
-		event.preventDefault();
-		//Acá se agregaría la lógica de autenticación mas fuerte
-		//Para el trabajo práctico voy a asumir que el login es exitoso
-		const Toast = Swal.mixin({
-			toast: true,
-			position: 'top-end',
-			showConfirmButton: false,
-			timer: 1500,
-			timerProgressBar: true,
-			didOpen: (toast) => {
-				toast.onmouseenter = Swal.stopTimer;
-				toast.onmouseleave = Swal.resumeTimer;
-			},
-		});
-		Toast.fire({
-			icon: 'success',
-			title: 'Signed in successfully',
-		});
-
-		loginModal.hide();
-		navItemAdmin.classList.remove('disabled');
-		navItemAdmin.classList.add('enabled');
-		loginButton.classList.remove('enabled');
-		loginButton.classList.add('disabled');
-		localStorage.setItem('login', true);
-
-		// navItemAdmin.href = '#';
-	});
-
-	//Acá manejo lo que pasa al clickear la opcion de cerrar sesión
-	logoutButton.addEventListener('click', () => {
-		localStorage.removeItem('login');
-		navItemAdmin.classList.remove('enabled');
-		navItemAdmin.classList.add('disabled');
-	});
+//Se abre la ventana modal cuando se hace click en la opción del menú "Login"
+loginButton.addEventListener('click', () => {
+	loginModal.show();
 });
+
+//Acá se hace el manejo del formulario de login, al hacer submit
+loginForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	//Acá se agregaría la lógica de autenticación mas fuerte
+	//Para el trabajo práctico voy a asumir que el login es exitoso
+	const Toast = Swal.mixin({
+		toast: true,
+		position: 'top-end',
+		showConfirmButton: false,
+		timer: 1500,
+		timerProgressBar: true,
+		didOpen: (toast) => {
+			toast.onmouseenter = Swal.stopTimer;
+			toast.onmouseleave = Swal.resumeTimer;
+		},
+	});
+	Toast.fire({
+		icon: 'success',
+		title: 'Signed in successfully',
+	});
+
+	loginModal.hide();
+	navItemAdmin.classList.remove('disabled');
+	navItemAdmin.classList.add('enabled');
+	loginButton.classList.remove('enabled');
+	loginButton.classList.add('disabled');
+	localStorage.setItem('login', true);
+	// Llamo a la función que controla inactividad
+	inactivityTime();
+
+	// navItemAdmin.href = '#';
+});
+
+//Acá manejo lo que pasa al clickear la opcion de cerrar sesión
+logoutButton.addEventListener('click', () => {
+	localStorage.removeItem('login');
+	navItemAdmin.classList.remove('enabled');
+	navItemAdmin.classList.add('disabled');
+});
+// });
